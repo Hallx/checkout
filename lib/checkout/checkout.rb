b354771be36975c61572906a72a48421f32fb80d
hallx <faheem+github@gmail.com>
2014-07-05 22:25:11 +0200

    checkout should initialize with some promotional rules

--- lib/checkout/checkout.rb
+++ lib/checkout/checkout.rb
@@ -1,3 +1,5 @@
 class Checkout::Checkout
   #here
+  def initialize(rules)
+  end
 end
--- spec/checkout/checkout_spec.rb
+++ spec/checkout/checkout_spec.rb
@@ -1,8 +1,13 @@
 require 'checkout'
 
 describe Checkout do
-  it 'should return correct version string' do
-    Checkout::Checkout.new
-    expect(2 + 2).to eq(4)
+  let(:promotional_rules) { {} }
+
+  describe '.new' do
+
+    it "should initialize with promotional rules" do
+      expect{ Checkout::Checkout.new(promotional_rules) }.not_to raise_error
+    end
   end
+
 end
