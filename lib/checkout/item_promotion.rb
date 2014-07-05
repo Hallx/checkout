a0ffb81089d7814012e66f17d852b60d5363a66a
hallx <faheem+github@gmail.com>
2014-07-06 00:12:20 +0200

    moved #filter_items to parent class

--- lib/checkout/item_promotion.rb
+++ lib/checkout/item_promotion.rb
@@ -15,10 +15,5 @@ module Checkout
       end
     end
 
-    private
-
-    def filter_items(items)
-      items.select { |item| @item == item }
-    end
   end
 end
--- lib/checkout/promotion.rb
+++ lib/checkout/promotion.rb
@@ -4,5 +4,11 @@ module Checkout
     def apply
       raise "Abstract class method called: Implement me in subclass"
     end
+
+    private
+
+    def filter_items(items)
+      items.select { |item| @item == item }
+    end
   end
 end
