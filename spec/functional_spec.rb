4efaa9c4c9e6df53062f321e2b0e43e260388e29
hallx <faheem+github@gmail.com>
2014-07-06 04:17:39 +0200

    added acceptance specs

--- spec/functional_spec.rb
+++ spec/functional_spec.rb
@@ -7,18 +7,39 @@ describe Checkout do
 
   let(:item_promotion)    { FactoryGirl.build(:item_card_holder_promotion, item: item_travel_card) }
   let(:total_promotion)   { Checkout::TotalPromotion.new("60", 10) }
+  let(:promotional_rules) { { total: [ total_promotion ], item: [ item_promotion ] } }
+
+  let(:checkout)          { Checkout::Checkout.new(promotional_rules) }
 
   describe "promotional_rules" do
     context "total promotion" do
-      let(:promotional_rules) { { total: [ total_promotion ] } }
-      let(:checkout)          { Checkout::Checkout.new(promotional_rules) }
-
       it "should lower the total" do
         checkout.scan item_travel_card
         checkout.scan item_cufflinks
         checkout.scan item_tshirt
 
         expect( checkout.total ).to eq(66.78)
+      end
+    end
+
+    context "item promotion" do
+      it "should lower the individual price of item" do
+        checkout.scan item_travel_card
+        checkout.scan item_tshirt
+        checkout.scan item_travel_card
+
+        expect( checkout.total ).to eq(36.95)
+      end
+    end
+
+    context "total and item promotion" do
+      it "should lower the individual price of item as well as should lower the total" do
+        checkout.scan item_travel_card
+        checkout.scan item_cufflinks
+        checkout.scan item_travel_card
+        checkout.scan item_tshirt
+
+        expect( checkout.total ).to eq(73.76)
       end
     end
   end
